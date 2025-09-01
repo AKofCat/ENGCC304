f0be1b916bdcb6bfd4529b87755386567876c5be
HomeLess <55099526+AKofCat@users.noreply.github.com>
2025-09-01 23:15:32 +0700

    Hwwwww

---
+++ Lap/Lap06/Hw6Loopfor.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int NUM;
+
+    printf("put number: ");
+    scanf("%d", &NUM);
+
+    for (int i = NUM; i >= 2; i--) {
+        int j;
+
+  
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                break;
+            }
+        }
+
+        if (j > i / 2) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+
