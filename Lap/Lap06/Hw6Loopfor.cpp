026a12fb0a30f5f13dc66bbe990de128f1c8b67a
HomeLess <55099526+AKofCat@users.noreply.github.com>
2025-09-02 18:49:56 +0700

    Niigger

--- Lap/Lap06/Hw6Loopfor.cpp
+++ Lap/Lap06/Hw6Loopfor.cpp
@@ -9,18 +9,15 @@ int main() {
     for (int i = NUM; i >= 2; i--) {
         int j;
 
-  
         for (j = 2; j <= i / 2; j++) {
             if (i % j == 0) {
                 break;
             }
         }
-
         if (j > i / 2) {
             printf("%d ", i);
+        
         }
-    }
-
     return 0;
 }
 
